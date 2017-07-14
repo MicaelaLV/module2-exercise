ca79c496aad8a4e87914b7fea484f7fab9293727
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-14 21:07:18 +0200

    public index route working

--- starter-code/routes/auth-routes.js
+++ starter-code/routes/auth-routes.js
@@ -4,17 +4,18 @@ const Customer = require("../models/customer");
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 
+
+// -----------------------------------------------SIGN UP
 authRoutes.get("/signup", (req, res, next) => {
     res.render("auth/signup");
 });
-
  //encrypt password 
 authRoutes.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const salt     = bcrypt.genSaltSync(bcryptSalt);
   const hashPass = bcrypt.hashSync(password, salt);
-
+//creates new customer 
   const newCustomer  = Customer({
     username,
     password: hashPass
@@ -23,7 +24,7 @@ authRoutes.post("/signup", (req, res, next) => {
   if (username === "" || password === "") {
     res.render("auth/signup", {
         errorMessage : "Indicate a username and password to sign up"
-    })
+    });
     res.end();
   };
 //validate if customer already exists
@@ -37,28 +38,28 @@ authRoutes.post("/signup", (req, res, next) => {
       res.end();
     }
   });
-
+//redirect to index if there's an error in the fillup of a new customer
  newCustomer.save((err) => {
     res.redirect("/");
   });
 });
 
-
+//---------------------------------------------------LOGIN
 authRoutes.get("/login", (req, res, next) => {
     res.render("auth/login");
 });
-
+//require customer's data for login
 authRoutes.post("/login", (req, res, next) => {
   var username = req.body.username;
   var password = req.body.password;
-
- if (username === "" || password === "") {
-    res.render("auth/login", {
-      errorMessage: "Indicate a username and a password to sign up"
-    });
-    res.end();
-  }
-
+//checks if customers fills up the login form
+  if (username === "" || password === "") {
+      res.render("auth/login", {
+        errorMessage: "Indicate a username and a password to sign up"
+      });
+      res.end();
+    };
+//check if the customer username exists
  Customer.findOne({ "username": username }, (err, customer) => {
       if (err || !customer) {
         res.render("auth/login", {
--- starter-code/routes/site-routes.js
+++ starter-code/routes/site-routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const siteRoutes = express.Router();
 
+//INDEX PAGE
 siteRoutes.get("/", (req, res, next) => {
     res.render('index');
 });
 
+//CUSTOMER SEARCH PAGE
+siteRoutes.get("/search", (req, res, next) => {
+    res.render('search');
+});
 
+//CUSTOMER LOGIN PAGE
 siteRoutes.use((req, res, next) => {
     if(req.session.currentCustomer) {
         next();
@@ -13,6 +19,7 @@ siteRoutes.use((req, res, next) => {
         res.redirect("/login");
     }
 });
+
 
 siteRoutes.get("/private", (req, res, next) => {
     res.render('private');
