a10c3042c8a07189cbfb247f7a696fdf444b19cb
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-15 16:31:42 +0200

    customer login and signup working correctly no bugs whatsoever

--- starter-code/routes/auth-routes.js
+++ starter-code/routes/auth-routes.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 
 
-// -----------------------------------------------SIGN UP
+// ---------------------------------------------------SIGN UP
 authRoutes.get("/signup", (req, res, next) => {
     res.render("auth/signup");
 });
@@ -25,8 +25,7 @@ authRoutes.post("/signup", (req, res, next) => {
     return res.render("auth/signup", {
         errorMessage : "Indicate a username and password to sign up"
     });
-  };
-  
+  }
   //validate if customer already exists
   Customer.findOne(
     { "username": username }, //search condition
@@ -36,18 +35,15 @@ authRoutes.post("/signup", (req, res, next) => {
         return res.render("auth/signup", {
           errorMessage: "The username already exists",
         }); 
-        // return res.redirect("/search");
-        //res.end();
       } else {
           newCustomer.save((err) => {
             return res.redirect("/search");
-          });
+          })
       }
-    });
-//redirect to index if there's an error in the fillup of a new customer
+    })
  });
 
-//---------------------------------------------------LOGIN
+//-----------------------------------------------------LOGIN
 authRoutes.get("/login", (req, res, next) => {
     res.render("auth/login");
 });
@@ -55,22 +51,18 @@ authRoutes.get("/login", (req, res, next) => {
 authRoutes.post("/login", (req, res, next) => {
   var username = req.body.username;
   var password = req.body.password;
-//checks if customers fills up the login form
+  //checks if customers fills up the login form
   if (username === "" || password === "") {
-      res.render("auth/login", {
+      return res.render("auth/login", {
         errorMessage: "Indicate a username and a password to sign up"
       });
-      return;
-      //res.end();
-    };
-//check if the customer username exists
- Customer.findOne({ "username": username }, (err, customer) => {
+  }
+  //check if the customer username exists
+  Customer.findOne({ "username": username }, (err, customer) => {
       if (err || !customer) {
-        res.render("auth/login", {
+       return res.render("auth/login", {
           errorMessage: "This username doesn't exist"
-        });
-        return;
-        //res.end();
+        })
       }
       if (bcrypt.compareSync(password, customer.password)) {
         // Save the login in the session!
@@ -81,7 +73,6 @@ authRoutes.post("/login", (req, res, next) => {
           errorMessage: "Incorrect password"
         });
         return;
-        //res.end();
       }
   });
 });
